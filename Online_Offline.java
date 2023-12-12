9173287988662f90b2ac31fbdc7dd2bee4aeaa05
shiqi0515 <106945511+shiqi0515@users.noreply.github.com>
2023-12-12 11:17:21 +0100

    Create Online_Offline.java

---
+++ Online_Offline.java
@@ -0,0 +1,73 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public enum UserState {
+    LoggedIn,
+    Online,
+    Offline
+}
+
+public class User {
+    private UserState state;
+    private List<LoginUser> loginUsers; // 0-to-many relationship with LoginUser
+    private List<OfflineUser> offlineUsers; // 0-to-many relationship with OfflineUser
+
+    // Getter for loginUsers
+    public List<LoginUser> getLoginUsers() {
+        return loginUsers;
+    }
+
+    // Setter for loginUsers
+    public void setLoginUsers(List<LoginUser> loginUsers) {
+        this.loginUsers = loginUsers;
+    }
+
+    // Getter for offlineUsers
+    public List<OfflineUser> getOfflineUsers() {
+        return offlineUsers;
+    }
+
+    // Setter for offlineUsers
+    public void setOfflineUsers(List<OfflineUser> offlineUsers) {
+        this.offlineUsers = offlineUsers;
+    }
+
+    // Method to add a LoginUser
+    public void addLoginUser(LoginUser loginUser) {
+        if (this.loginUsers == null) {
+            this.loginUsers = new ArrayList<>();
+        }
+        this.loginUsers.add(loginUser);
+    }
+
+    // Method to add an OfflineUser
+    public void addOfflineUser(OfflineUser offlineUser) {
+        if (this.offlineUsers == null) {
+            this.offlineUsers = new ArrayList<>();
+        }
+        this.offlineUsers.add(offlineUser);
+    }
+}
+
+
+public class OfflineUser extends User {
+
+}
+
+public class LoginUser extends User {
+    private int bankID;
+    private int phoneNum;
+    private Address address;
+    private String email;
+    
+}
+
+public class HealthData {
+    private float height;
+    private float weight;
+    private float bloodPressure;
+    private float bodyTemperature;
+    private float respiratoryRate;
+    private int heartRate;
+    
+}
